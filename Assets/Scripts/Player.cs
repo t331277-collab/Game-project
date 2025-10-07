f20bf17a0fdebc3e6ba9291a91a1640b225f24fb
t331277 <t331277@gmail.com>
2025-10-07 20:54:46 +0900

    스크립트 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
