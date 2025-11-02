b9f5316e32039174c51d8dfc933c5957aaf8b16d
t331277 <t331277@gmail.com>
2025-11-02 22:00:22 +0900

    새로운 캐릭터 생성 및 HitBox 구현
    매우 간단한 로직을 사용함
    말로 설명하면
    Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(pos.position, boxSize, 0)
    
    를 사용해 HItBox를 만들고 Z 키를 누르면 히트박스가 활성화가 되어서 안에 Enemy tag  를 가진 Object가 있으면 Player 에서 인식

--- Assets/Scripts/Player_Attack.cs
+++ Assets/Scripts/Player_Attack.cs
@@ -2,15 +2,58 @@ using UnityEngine;
 
 public class Player_Attack : MonoBehaviour
 {
+
+    Rigidbody2D rgd;
+    Animator animator;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rgd = GetComponent<Rigidbody2D>();
     }
+
+    private float curTime;
+    public float coolTime = 0.5f;
+    public Transform pos;
+    public Vector2 boxSize;
 
     // Update is called once per frame
     void Update()
     {
+        //'Z'버튼을 공격
+        
+        if (curTime <= 0)
+        {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                animator.SetTrigger("Attack");
+                Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(pos.position, boxSize, 0);
+
+                foreach (Collider2D collider in collider2Ds)
+                {
+                    Debug.Log(collider.tag);
+                
+                }
+
+                curTime = coolTime;
+            }
+                //공격  
+
+            //Debug.Log("Z");
+        }
+        else
+        {
+            curTime -= Time.deltaTime;
+        }
         
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(pos.position, boxSize);
+    }
+
 }
